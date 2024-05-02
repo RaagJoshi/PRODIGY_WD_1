971f16e1327b68db8fff952669e46134cd70812f
Raag Joshi <raagjoshi67@gmail.com>
2024-05-02 21:22:13 +0530

    added author name

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import Menu from "../components/Menu";
 function Home() {
   return (
     <>
-      <Menu content="This is the Home Page. The entry point of the website." />
+      <Menu content="This is the Home Page. The entry point of the website. Author is Raag Joshi" />
     </>
   );
 }
