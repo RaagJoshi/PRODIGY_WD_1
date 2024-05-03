08c2873548d9427875b21f71a93e81a238b057c4
Raag Joshi <raagjoshi67@gmail.com>
2024-05-03 16:13:49 +0530

    Color change on scroll

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,9 +1,26 @@
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function Menu({ content }: string) {
+  const [navbar, setNavbar] = useState(false);
+
+  const changeBackground = () => {
+    console.log(window.scrollY);
+    if (window.scrollY >= 66) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
+
+  useEffect(() => {
+    changeBackground();
+    window.addEventListener("scroll", changeBackground);
+  });
+
   return (
     <>
-      <div className="navbar">
+      <div className={navbar ? "navbar-active" : "navbar"}>
         <Link className="link" to="/">
           Home
         </Link>
@@ -24,5 +41,4 @@ function Menu({ content }: string) {
     </>
   );
 }
-
 export default Menu;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import Menu from "../components/Menu";
 function Home() {
   return (
     <>
-      <Menu content="This is the Home Page. The entry point of the website. Author is Raag Joshi" />
+      <Menu content="This is the Home Page. The entry point of the website. Scroll down and navbar will change its color. Author is Raag Joshi." />
     </>
   );
 }
